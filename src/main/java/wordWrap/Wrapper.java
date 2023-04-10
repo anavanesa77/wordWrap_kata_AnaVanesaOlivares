582b6886529977250bf1d75c2bef63b7a9f58964
anavanesa77 <anav.olivares@gmail.com>
2023-04-10 16:32:07 +0100

    First Commit

---
+++ src/main/java/wordWrap/Wrapper.java
@@ -0,0 +1,8 @@
+package wordWrap;
+
+
+public class Wrapper{
+    public static String Wrap(String text, int columnNumber) {
+        return "Cero";
+    }
+}
---
+++ src/main/java/wordWrap/WrapperShould.java
@@ -0,0 +1,5 @@
+package wordWrap;
+
+class WrapperShould {
+
+}
