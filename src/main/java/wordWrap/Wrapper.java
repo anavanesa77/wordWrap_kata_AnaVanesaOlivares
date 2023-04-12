1d76d44acce3dc09218ded5b125cb9d12ab1281d
anavanesa77 <anav.olivares@gmail.com>
2023-04-12 11:05:09 +0100

    :blue_square: return_an_exception_if_the_column_number_is_negative

--- src/main/java/wordWrap/Wrapper.java
+++ src/main/java/wordWrap/Wrapper.java
@@ -4,17 +4,13 @@ package wordWrap;
 public class Wrapper {
     public static String wrap(String text, int columnNumber) throws ExceptionWrapper {
 
-        if (text.equals("")) {
-            var errorMessage = createErrorMessage();
+        if ((text.equals("")) || (columnNumber <= 0)) {
+            var errorMessage = "format or extension error";
             throw new ExceptionWrapper(errorMessage);
         }
 
-        return "Casa";
+        return "Happy";
     }
 
-    private static String createErrorMessage() {
-        var errorMessage = "format or extension error";
-        return errorMessage;
-    }
 }
 
